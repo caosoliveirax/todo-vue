a3d4e13ad25f58b120b05d711eae53310fc71b94
Carlos Oliveira <kaduchaos18@gmail.com>
2025-08-01 00:27:10 -0300

    create app components

---
+++ src/components/Todo.vue
@@ -0,0 +1,32 @@
+<script setup>
+    const props = defineProps(['tarefas']);
+
+    const temTarefaPendente = () => props.tarefas.length > 0 
+</script>
+
+
+<template>
+    <ul v-if="temTarefaPendente()" class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas">
+            <input @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada"
+            :id="tarefa.titulo" type="checkbox">
+            <label :class="{ done: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+    <span v-else class="message">Não há tarefas pendentes</span>
+</template>
+
+<style scoped>
+
+    .done {
+    color: orange;
+    text-decoration: line-through;
+    }
+
+    .message {
+        font-size: 4rem;
+        font-style: italic;
+    }
+</style>
